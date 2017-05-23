84919d8eb5c4c8b41e2eb698b0eaac46a87bba5b
Andres Sevillano <andres@chameleon.ad>
2017-05-23 18:16:13 +0100

    Stop crush due to sprintf

--- test/nginx_test.cpp
+++ test/nginx_test.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <cstring>
 
 // just logging something ( --log_level=message )
 #include <boost/test/unit_test.hpp>
@@ -23,11 +24,14 @@ static std::atomic<unsigned> VALUE {0};
 void handle_request(FCGX_Request *request) {
 	char *input;
 	unsigned value;
+	char buffer[1024];
+	std::memset(buffer, '\0', 1024);
 
 	printf("Content-Type: text/plain\r\n\r\n");
-	if ((input = get_param("QUERY_STRING")) != NULL) { 
-		printf(input); // manual test in a browser
-		if( 1 == sprintf(input, "?value=%u", &value) ) { VALUE.store(value); } 
+	if ((input = get_param("QUERY_STRING")) != NULL) {
+		std::memcpy(buffer, input, 16);
+		printf(buffer); // manual test in a browser
+		if( 1 == sprintf(buffer, "?value=%u", &value) ) { VALUE.store(value); }
 	}
 	printf("\n");
 }
@@ -49,6 +53,8 @@ void server() {
 	int sock;
 	FCGX_Request request;
 
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
 	FCGX_Init(); 
 	sock = FCGX_OpenSocket(":2005", 5);
 	FCGX_InitRequest(&request, sock, 0);
@@ -63,7 +69,9 @@ void server() {
 }
 
 void client() {
-	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
 	if( check_response("http://0.0.0.0/testingFCGX?value=1", CURLE_OK) ) { 
  		BOOST_TEST_MESSAGE( "Seems OK");
 		SUCCESS.store(true);
@@ -80,6 +88,7 @@ BOOST_AUTO_TEST_CASE( test001 ) {
  std::thread client_thread(client);
  server_thread.join();
  client_thread.join();
+
  BOOST_CHECK( SUCCESS.load() );
 }
 
