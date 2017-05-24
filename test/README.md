85ec49a27867b52a0f293531779b2d22e63c5a8c
Andres Sevillano <andres@chameleon.ad>
2017-05-24 12:15:32 +0100

    Added commandline arguments

--- test/README.md
+++ test/README.md
@@ -2,30 +2,49 @@
 
 Improved FastCGI library tests. Pay attention to its testing ports, e.g. **2006**.
 
+In order to build tests and running them, use typical command:
+
+	rm -rf build && mkdir -p build && cd build && cmake .. -DModifiedFastCGI_NGINX=1 && make && ctest
+
 ### NGINX
 
-Don't forget to add to your **nginx** configuration file:
+Don't forget to add to your **nginx** configuration file if you choose using NGINX as FastCGI server provider. You can invoke its help for further info:
 
-      location /testingFCGX  {
-              fastcgi_pass   0.0.0.0:2006;
-              fastcgi_connect_timeout 5h;
-              fastcgi_read_timeout 5h;
+        ./nginx_fastcgi_test --log_level=message -- --help
 
-              fastcgi_param  QUERY_STRING       $query_string;
-              fastcgi_param  REQUEST_METHOD     $request_method;
-              fastcgi_param  CONTENT_TYPE       $content_type;
-              fastcgi_param  CONTENT_LENGTH     $content_length;
-              fastcgi_param  REQUEST     	$request;
-              fastcgi_param  REQUEST_BODY     	$request_body;
-              fastcgi_param  REQUEST_URI        $request_uri;
-              fastcgi_param  DOCUMENT_URI       $document_uri;
-              fastcgi_param  DOCUMENT_ROOT      $document_root;
-              fastcgi_param  SERVER_PROTOCOL    $server_protocol;
-              fastcgi_param  REMOTE_ADDR        $remote_addr;
-              fastcgi_param  REMOTE_PORT        $remote_port;
-              fastcgi_param  SERVER_ADDR        $server_addr;
-              fastcgi_param  SERVER_PORT        $server_port;
-              fastcgi_param HTTP_REFERER      	$http_referer;
-              fastcgi_param SCHEME              $scheme;
-        }
+        Usage ./nginx_fastcgi_test [--log_level=message] -- [--help] [--nginx_location=NGINX configured location at nginx.conf] [--fastcgi_server=ip for testing server] [--fastcgi_port=port for testing server]
+
+        All those commandline arguments are optional provided NGINX is properly configured and running:
+
+          By default, --nginx_location="http://0.0.0.0/testingFCGX
+
+          By default, --fastcgi_server=0.0.0.0
+
+          By default, --fastcgi_port=2006
+
+        Example of NGINX configuration:
+
+            location /testingFCGX  {
+
+                    fastcgi_pass   0.0.0.0:2006;
+                    fastcgi_connect_timeout 5h;
+                    fastcgi_read_timeout 5h;
+
+                    fastcgi_param  QUERY_STRING       $query_string;
+                    fastcgi_param  REQUEST_METHOD     $request_method;
+                    fastcgi_param  CONTENT_TYPE       $content_type;
+                    fastcgi_param  CONTENT_LENGTH     $content_length;
+                    fastcgi_param  REQUEST            $request;
+                    fastcgi_param  REQUEST_BODY       $request_body;
+                    fastcgi_param  REQUEST_URI        $request_uri;
+                    fastcgi_param  DOCUMENT_URI       $document_uri;
+                    fastcgi_param  DOCUMENT_ROOT      $document_root;
+                    fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+                    fastcgi_param  REMOTE_ADDR        $remote_addr;
+                    fastcgi_param  REMOTE_PORT        $remote_port;
+                    fastcgi_param  SERVER_ADDR        $server_addr;
+                    fastcgi_param  SERVER_PORT        $server_port;
+                    fastcgi_param HTTP_REFERER        $http_referer;
+                    fastcgi_param SCHEME              $scheme;
+              }
 
--- test/nginx_test.cpp
+++ test/nginx_test.cpp
@@ -1,34 +1,124 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE NgnixUnitTest
 
-#include <cstdio>
-#include <cstdlib>
-#include <curl/curl.h>
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <string>
+#include <regex>
+#include <map>
+
+#include <curl/curl.h>
 
 // just logging something ( --log_level=message )
 #include <boost/test/unit_test.hpp>
 
 #include "fcgiapp.h"
 
-#define printf(...) FCGX_FPrintF(request->out, __VA_ARGS__)
-#define get_param(KEY) FCGX_GetParam(KEY, request->envp)
-
-// global out of lazyness
+// globalish public vars out of lazyness
+static std::string nginxLocation {"http://0.0.0.0/testingFCGX"};
+static std::string fastcgiServer {"0.0.0.0"};
+static std::string fastcgiPort {"2006"};
 static std::atomic<bool> SUCCESS {false};
 static std::atomic<unsigned> VALUE {0};
 
+// command line info
+struct GlobalInit {
+
+  using Param = std::map<std::string, std::string>;
+
+  GlobalInit() : argc(boost::unit_test_framework::framework::master_test_suite().argc),
+		 argv(boost::unit_test_framework::framework::master_test_suite().argv)
+  {
+    Param param {commandline(argc, argv)};
+
+    if(param["help"] == "help") { BOOST_TEST_MESSAGE("\n\tUsage " << param["PROGRAM"] <<
+R"( [--log_level=message] -- [--help] [--nginx_location=NGINX configured location at nginx.conf] [--fastcgi_server=ip for testing server] [--fastcgi_port=port for testing server]
+
+	All those commandline arguments are optional provided NGINX is properly configured and running:
+
+	  By default, --nginx_location="http://0.0.0.0/testingFCGX
+
+	  By default, --fastcgi_server=0.0.0.0
+
+	  By default, --fastcgi_port=2006
+
+	Example of NGINX configuration:
+
+	    location /testingFCGX  {
+
+		    fastcgi_pass   0.0.0.0:2006;
+		    fastcgi_connect_timeout 5h;
+		    fastcgi_read_timeout 5h;
+
+		    fastcgi_param  QUERY_STRING       $query_string;
+		    fastcgi_param  REQUEST_METHOD     $request_method;
+		    fastcgi_param  CONTENT_TYPE       $content_type;
+		    fastcgi_param  CONTENT_LENGTH     $content_length;
+		    fastcgi_param  REQUEST            $request;
+		    fastcgi_param  REQUEST_BODY       $request_body;
+		    fastcgi_param  REQUEST_URI        $request_uri;
+		    fastcgi_param  DOCUMENT_URI       $document_uri;
+		    fastcgi_param  DOCUMENT_ROOT      $document_root;
+		    fastcgi_param  SERVER_PROTOCOL    $server_protocol;
+		    fastcgi_param  REMOTE_ADDR        $remote_addr;
+		    fastcgi_param  REMOTE_PORT        $remote_port;
+		    fastcgi_param  SERVER_ADDR        $server_addr;
+		    fastcgi_param  SERVER_PORT        $server_port;
+		    fastcgi_param HTTP_REFERER        $http_referer;
+		    fastcgi_param SCHEME              $scheme;
+	      }
+
+)"); exit(0); }
+
+    if( param.cend() != param.find("nginx_location") ) { nginxLocation = param["nginx_location"]; }
+    if( param.cend() != param.find("fastcgi_server") ) { fastcgiServer = param["fastcgi_server"]; }
+    if( param.cend() != param.find("fastcgi_port") ) { fastcgiPort = param["fastcgi_port"]; }
+
+    BOOST_TEST_MESSAGE( "\nNGINX Location: " << nginxLocation);
+    BOOST_TEST_MESSAGE( "FastCGI Server: " << fastcgiServer);
+    BOOST_TEST_MESSAGE( "FastCGI Port:   " << fastcgiPort << "\n");
+  }
+
+  Param commandline(const int argc = 0, char** argv = nullptr) const
+  {
+	  Param result{};
+	  if(argc > 0) { result.emplace("PROGRAM", argv[0]); }
+
+	  static const std::regex param_pattern{"^-[-]?(.*)[ ]*=[ ]*(.*)$"};
+	  static const std::regex help_pattern{"^-[-]?help$"};
+	  for(int i=1; i<argc; ++i) {
+		 std::string candidate{argv[i]};
+		 std::smatch param_match;
+		 if(std::regex_match(candidate, param_match, help_pattern)) {
+			 result.emplace("help", "help");
+			 continue;
+		 }
+		 if(std::regex_match(candidate, param_match, param_pattern)) {
+			 if( param_match.size() == 3) {
+				 result.emplace(param_match[1].str(), param_match[2].str());
+			 }
+		 }
+	  }
+
+	  return result;
+  }
+
+private:
+  int argc {};
+  char **argv {nullptr};
+};
+BOOST_GLOBAL_FIXTURE( GlobalInit );
+
 void handle_request(FCGX_Request *request) {
 	char *input;
-	printf("Content-Type: text/plain\r\n\r\n");
-	if ((input = get_param("QUERY_STRING")) != NULL) {
+	FCGX_FPrintF(request->out, "Content-Type: text/plain\r\n\r\n");
+	if ((input = FCGX_GetParam("QUERY_STRING", request->envp)) != NULL) {
 		unsigned value;
-		printf(input); // manual test in a browser
+		FCGX_FPrintF(request->out, input); // manual test in a browser
 		if( 1 == sscanf(input, "value=%u", &value) ) { VALUE.store(value); }
 	}
-	printf("\n");
+	FCGX_FPrintF(request->out, "\n");
 }
 
 bool check_response(const char* const query, const CURLcode expected) {
@@ -49,7 +139,7 @@ void server() {
 	FCGX_Request request;
 
 	FCGX_Init(); 
-	sock = FCGX_OpenSocket(":2006", 5);
+	sock = FCGX_OpenSocket((fastcgiServer + ":" + fastcgiPort).c_str(), 5);
 	FCGX_InitRequest(&request, sock, 0);
 
 	// just in case 
@@ -66,7 +156,7 @@ void client() {
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-	if( check_response("http://0.0.0.0/testingFCGX?value=1", CURLE_OK) ) { 
+	if( check_response((nginxLocation + "?value=1").c_str(), CURLE_OK) ) {
  		BOOST_TEST_MESSAGE( "Seems OK");
 		SUCCESS.store(true);
 	} else { 
@@ -76,6 +166,7 @@ void client() {
 }
 
 BOOST_AUTO_TEST_CASE( test001 ) {
+
  BOOST_TEST_MESSAGE( "\ntest001: Just a hello world library check");
 
  std::thread server_thread(server);
